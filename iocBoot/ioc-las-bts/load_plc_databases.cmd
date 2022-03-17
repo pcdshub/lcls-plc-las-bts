62c70adafc30a36b271261ddba014eec93be35b5
Ken Lauer <klauer@slac.stanford.edu>
2022-03-17 10:12:12 -0700

    MNT: rebuild IOC after symbol removal

--- iocBoot/ioc-las-bts/load_plc_databases.cmd
+++ iocBoot/ioc-las-bts/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 5565
-callbackSetQueueSize(13130)
+# Total records: 5562
+callbackSetQueueSize(13124)
 dbLoadRecords("las_bts.db", "PORT=ASYN_PLC,SIM=,BTPS=LTLHN:BTPS,PREFIX=PLC:LAS:BTS:,IOCNAME=$(IOC),IOC=$(IOC)")
