51c9d640527b4c020a791fa28b58979d0b2c23a4
Ken Lauer <klauer@slac.stanford.edu>
2022-05-20 13:59:25 -0700

    MNT: rebuild IOC with dev pytmc

--- iocBoot/ioc-las-bts/load_plc_databases.cmd
+++ iocBoot/ioc-las-bts/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 4058
-callbackSetQueueSize(10116)
+# Total records: 4089
+callbackSetQueueSize(10178)
 dbLoadRecords("las_bts.db", "PORT=ASYN_PLC,SIM=,BTPS=LTLHN:BTPS,PREFIX=PLC:LAS:BTS:,IOCNAME=$(IOC),IOC=$(IOC)")
